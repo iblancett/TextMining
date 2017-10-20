58c12c65a49ebe1fac89c74185b4860feba46cd3
Isabel Blancett <isabel.blancett@students.olin.edu>
2017-10-19 23:27:25 -0400

    MP3 Revisions

--- states.py
+++ states.py
@@ -6,36 +6,9 @@ YOUR HEADER COMMENT HERE
 
 """
 
-# Assumptions:
-# Wikipedia articles of states are up to date
-# Distance between states = google maps distance between states
-# If states are next to each other, their distance is 0
-# Hawaii is considered 5000 miles from Alabama for the sake of this script
-
 import wikipedia
 from collections import Counter
-from itertools import chain
-import matplotlib.pyplot as plt
 
-states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-          'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia (U.S. state)',
-          'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
-          'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
-          'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
-          'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
-          'New Mexico', 'New York (state)', 'North Carolina', 'North Dakota', 'Ohio',
-          'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
-          'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-          'Virginia', 'Washington (state)', 'West Virginia', 'Wisconsin', 'Wyoming']
-
-# In miles
-dist_al = [0, 4303, 1564, 446, 2161, 1444, 1157, 932, 0, 0,
-           5000, 2131, 700, 614, 948, 1009, 500, 388, 1507, 869,
-           1262, 935, 1324, 0, 609, 1964, 1166, 2084, 1325, 1028,
-           1228, 1211, 585, 1620, 728, 728, 2422, 981, 1225, 395,
-           1315, 0, 828, 1773, 1360, 669, 2547, 705, 934, 1650]
-
-freq_words = []
 
 def get_content(title):
     """ grabs the content of a wikipedia page
@@ -49,12 +22,12 @@ def get_content(title):
     article = wikipedia.page(title)
     return article.content
 
-def find_freq_words(title):
+def find_freq_words(title, freq_words):
     """ inserts word_freq list ordered by frequency & deletes punctuation
 
         title: name of the wikipedia page
         calls: get_content(title)
-        returns: nothing
+        returns: list of words ordered by frequency to be appended to freq_words
     """
 
     words = get_content(title).split()
@@ -73,10 +46,9 @@ def find_freq_words(title):
     # Orders by frequency without repeat terms
     seen = {}
     ordered_set = [seen.setdefault(x, x) for x in ordered if x not in seen]
-    freq_words.append(ordered_set)
-    return
+    return ordered_set
 
-def remove_matches():
+def remove_matches(numStates, freq_words):
     """ removes words that are present in 10 state lists (e.g. 'the')
 
         returns: nothing
@@ -91,62 +63,64 @@ def remove_matches():
 # Delete words that appear in 10 or more articles
     for word in hist.keys():
         if hist[word] >= 10:
-            for i in range(50):
+            for i in range(numStates):
                 if word in freq_words[i]:
                     freq_words[i].remove(word)
 
+    return freq_words
 
-def find_common_words(state1, state2):
-    """ Main Loop: compares two states
+
+def find_common_words(states):
+    """ Finds a list of uncommon words for each state
+
+        returns: updated freq_words list
+    """
+
+    freq_words = []
+
+    numStates = len(states)
+
+# make list of all words (no repeats per list)
+    for name in states:
+        ordered_set = find_freq_words(name, freq_words)
+        freq_words.append(ordered_set)
+        print(name)
+
+# get rid of matching words across state articles
+    freq_words = remove_matches(numStates, freq_words)
+    return freq_words
+
+def compare_states(state1,state2):
+    """ Compares uncommon words between two states
 
         returns: the common_words of two states
     """
-    for name in states:
-        find_freq_words(name)
-        print(name)
 
-    remove_matches()
+    states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+              'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia (U.S. state)',
+              'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+              'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+              'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
+              'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+              'New Mexico', 'New York (state)', 'North Carolina', 'North Dakota', 'Ohio',
+              'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
+              'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+              'Virginia', 'Washington (state)', 'West Virginia', 'Wisconsin', 'Wyoming']
 
+# Get all info for comparison
+    freq_words = find_common_words(states)
     common_words = []
     index1 = states.index(state1)
     index2 = states.index(state2)
 
-# Find the common words
+# Compare the states
     for item in freq_words[index1]:
         if item in freq_words[index2]:
             common_words.append(item)
 
-def comp_al():
-    """ compares Alabama to every state
-
-        plots: # of common words vs. distance
-    """
-
-    for name in states:
-        find_freq_words(name)
-        print(name)
-
-    remove_matches()
-
-    num_sim_al = []
-    for i in range(1,50):
-        common_words = []
-        index1 = 0
-        index2 = i
-
-        for item in freq_words[index1]:
-            if item in freq_words[index2]:
-                common_words.append(item)
-
-# Add number of common words to num_sim_al
-        num_sim_al.append(len(common_words))
-
-#plot
-    plt.plot(dist_al[1:], num_sim_al, 'ro')
-    plt.show()
-    return
-
-
+    print(len(common_words))
+    return common_words
 
 if __name__ == "__main__":
-    comp_al()
+    # example run
+    print(compare_states('Alabama', 'Vermont'))
